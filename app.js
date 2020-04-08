6a966ce0f9708c0340ae7a21c26525ca06f481ec
Ian Mihura <mihura.ian@gmail.com>
2020-04-08 23:37:05 +0200

    init help pag

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use('/checkin', router.get('/checkin', (req, res) => res.render('checkin')))
 app.use('/buy', router.get('/buy', (req, res) => res.render('buy')));
 app.use('/tickets', router.get('/tickets', (req, res) => res.render('tickets')));
 app.use('/txid', router.get('/txid', (req, res) => res.render('txid')));
+app.use('/help', router.get('/help', (req, res) => res.render('help')));
 
 // Server
 var mainService = require('./service/main');
